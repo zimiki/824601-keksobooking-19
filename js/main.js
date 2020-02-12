c42afbe191bcba7f6d78fcdfcc99a2ac4c4297e9
zimiki <marmonka@gmail.com>
2020-02-12 21:05:50 +0500

    Добавляет обработчики на открытие и закрытие карты

--- js/main.js
+++ js/main.js
@@ -209,6 +209,8 @@ var mapFilter = map.querySelector('.map__filters'); // Форма №1 .map__fil
 var adForm = document.querySelector('.ad-form'); // Форма №2 .ad-form
 var resetFormButton = adForm.querySelector('.ad-form__reset'); // Элемент сбрасывающий карту до изначального неативного состояния
 var mapPins = map.querySelector('.map__pins');
+var roomSelect = adForm.querySelector('#room_number');
+var capacitySelect = adForm.querySelector('#capacity');
 
 // Функция, которая УСТАНАВЛИВАЕТ неактивное состояние на <input> и <select> формы с помощью атрибута disabled
 var getInactiveForm = function (form) {
@@ -234,16 +236,22 @@ var getActiveForm = function (form) {
   }
 };
 
-// ???? ВОПРОС ???????   Функция, которая отвечает за связку двух селекторов
-var selectRoomNumber = adForm.querySelector('#room_number');
-var validityCapacity = function () {
-  var valueRoomNumber = selectRoomNumber.options[selectRoomNumber.selectedIndex].value; // Определим, какое значение выбрано
-  // console.log(valueRoomNumber);
-  if (valueRoomNumber === 1) { // ???ВОПРОС ???   Почему не работает
-  // console.log('ОДИН');
+// Валидация input room-capasity
+var validateCapacity = function () {
+  var rooms = parseInt(roomSelect.value, 10);
+  var capacity = parseInt(capacitySelect.value, 10);
+  if (rooms === 1 && capacity !== 1) {
+    roomSelect.setCustomValidity('для 1 гостя');
+  } else if (rooms === 2 && (capacity < 1 || capacity > 2)) {
+    roomSelect.setCustomValidity('для 2 гостей или для 1 гостя');
+  } else if (rooms === 3 && (capacity < 1 || capacity > 3)) {
+    roomSelect.setCustomValidity('для 3 гостей, для 2 гостей или для 1 гостя');
+  } else if (rooms === 100 && capacity !== 0) {
+    roomSelect.setCustomValidity('не для гостей');
+  } else {
+    roomSelect.setCustomValidity('');
   }
 };
-
 
 var openMap = function () {
   map.classList.remove('map--faded'); // Снимает неактивное состояние .map содержит класс map--faded;
@@ -252,21 +260,20 @@ var openMap = function () {
   getActiveForm(adForm); // Снимает неактивное состояние c формы №2
   var fragmentWithPins = getFragment(offers, renderMapPin); // Вызов функции создания фрагмента c pin объявлений
   map.querySelector('.map__pins').appendChild(fragmentWithPins); // Добавление фрагмента c pin в DOM
+  // Добавление обработчиков:
+  roomSelect.addEventListener('change', validateCapacity); // Валидация значений при смене количества комнат
+  capacitySelect.addEventListener('change', validateCapacity); // Валидация значений при смене количества гостей
   resetFormButton.addEventListener('click', closeMap); // Обработчик для перехода к начальному состоянию
-  selectRoomNumber.addEventListener('change', validityCapacity); // Обработчик события измения
 };
 
 // Функция, которая удаляет все вставленные фрагметом метки объявлений
 var removeNewMapPins = function () {
-  var newMapPins = mapPins.querySelectorAll('.map__pin'); //    ??? ВОПРОС???? !!!!! Как только я заворачиваю это в for (var i = newMapPins; i > 0; --i) перестает работать
-  mapPins.removeChild(newMapPins[8]);
-  mapPins.removeChild(newMapPins[7]);
-  mapPins.removeChild(newMapPins[6]);
-  mapPins.removeChild(newMapPins[5]);
-  mapPins.removeChild(newMapPins[4]);
-  mapPins.removeChild(newMapPins[3]);
-  mapPins.removeChild(newMapPins[2]);
-  mapPins.removeChild(newMapPins[1]);
+  var newMapPins = mapPins.querySelectorAll('.map__pin');
+  for (var i = 0; i < newMapPins.length; i++) {
+    if (!newMapPins[i].classList.contains('map__pin--main')) {
+      mapPins.removeChild(newMapPins[i]);
+    }
+  }
 };
 
 var closeMap = function () {
@@ -275,8 +282,10 @@ var closeMap = function () {
   getInactiveForm(mapFilter); // Заблокирована форма №1 .map__filters
   getInactiveForm(adForm); // Заблокирована форма №2 .ad-form
   removeNewMapPins();
+  // Снятие обработчиков:
+  roomSelect.removeEventListener('change', validateCapacity);
+  capacitySelect.removeEventListener('change', validateCapacity);
   resetFormButton.removeEventListener('click', closeMap);
-  selectRoomNumber.removeEventListener('change', validityCapacity);
 };
 
 // Функция, которыя устанавливает значения поля ввода адреса. Это координаты, на которые метка указывает своим острым концом.
@@ -292,7 +301,7 @@ var getСoordinatesAddress = function (evt) {
   inputAddress.value = addressX + ' ,' + addressY;
 };
 
-var getInfoMousedown = function (evt) {
+var mainPinLeftClikHandler = function (evt) {
   if (typeof evt === 'object') {
     switch (evt.button) { // Проверка на то что клик приходит с левой кнопки мыши
       case 0:
@@ -302,12 +311,13 @@ var getInfoMousedown = function (evt) {
   }
 };
 
+// Запускаем код:
 // Приводит страницу в изначальное неактивное состояние
 getInactiveForm(mapFilter); // Заблокирована форма №1 .map__filters
 getInactiveForm(adForm); // Заблокирована форма №2 .ad-form
 
 // Добавялет обработчик АКТИВАЦИИ на mousedown
-mainPin.addEventListener('mousedown', getInfoMousedown);
+mainPin.addEventListener('mousedown', mainPinLeftClikHandler);
 
 // Добавялет обработчик АКТИВАЦИИ на keydown с проверкой на то что это кнопка Enter
 mainPin.addEventListener('keydown', function (evt) {
