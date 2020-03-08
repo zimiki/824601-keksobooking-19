9f728647df4364a54068819cd2bfdb9c9856b17e
zimiki <marmonka@gmail.com>
2020-03-08 10:21:37 +0500

    Добавлены константы по цене

--- js/sort.js
+++ js/sort.js
@@ -7,6 +7,10 @@
   var NUMBER_PINS_ON_MAP = 5; // выводить на карту не более 5 меток
   var housingTypeSelect = mapFilter.querySelector('#housing-type'); // селект с типом жилья
   var housingPriceSelect = mapFilter.querySelector('#housing-price'); // селект с цены
+  var Price = {
+    LOW: 10000,
+    HIGH: 50000
+  };
 
 
   // Функция, которая не дает быть массиву длинее 5 элементов, возвращает массив
@@ -22,17 +26,17 @@
     switch (housingPriceSelect.value) {
       case 'low':
         sortPrice = arr.filter(function (offer) {
-          return offer.offer.price <= 10000;
+          return offer.offer.price <= Price.LOW;
         });
         break;
       case 'middle':
         sortPrice = arr.filter(function (offer) {
-          return (offer.offer.price > 10000 && offer.offer.price < 50000);
+          return (offer.offer.price > Price.LOW && offer.offer.price < Price.HIGH);
         });
         break;
       case 'high':
         sortPrice = arr.filter(function (offer) {
-          return offer.offer.price >= 50000;
+          return offer.offer.price >= Price.HIGH;
         });
         break;
       default:
